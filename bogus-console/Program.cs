ed738d8faab354b863bd8d3bd446f3a5fc921416
Prateek Sahay <prateeksahay96@gmail.com>
2018-12-12 21:41:19 +0530

    Bogus-learning

---
+++ bogus-console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Bogus;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace bogus_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var ans= GetSampleTableData();
+           
+           foreach(var item in ans)
+           {
+                Console.WriteLine(item.topicId);
+                Console.WriteLine(item.topicName);
+           }
+        }
+
+        private static List<Topic> GetSampleTableData()
+        {
+
+            var userFaker = new Faker<Topic>()
+            //.RuleFor(t => t.topicId, f => f.IndexFaker)
+            .RuleFor(t => t.topicId, f => f.Name.FirstName())
+            .RuleFor(t => t.topicName, f => f.Internet.Avatar());
+            var users = userFaker.Generate(2);
+           // string full=new Bogus.DataSets.Name().FullName();
+           // Console.WriteLine(""+full);
+            Topic obj1=userFaker;
+            //Console.WriteLine(users.DumpAsJson());
+
+            return users;
+        }
+    }
+}
---
+++ bogus-console/topic.cs
@@ -0,0 +1,11 @@
+namespace bogus_console
+{
+
+    public class Topic
+    {
+        public string topicId { get; set; }
+        public string topicName { get; set; }
+    
+    }
+
+}
